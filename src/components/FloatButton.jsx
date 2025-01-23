64bd3b051f5c16f090bc3855bcee91c6ffb6c4ed
Talal Mahmud <mahmudtalal2@gmail.com>
2025-01-23 11:21:15 +0600

    update button added

---
+++ src/components/FloatButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./floatbutton.css";
+const FloatButton = () => {
+  return (
+    <button class="floating-btn">
+      <a
+        href="https://talalmahmud.vercel.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        updated
+      </a>
+    </button>
+  );
+};
+
+export default FloatButton;
